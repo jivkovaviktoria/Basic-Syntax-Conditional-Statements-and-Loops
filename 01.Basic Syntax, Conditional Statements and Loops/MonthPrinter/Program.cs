d6bfe275eb8a215f684b5a7ec85afc9e46060593
Viktoriya Jivkova <85957657+jivkovaviktoria@users.noreply.github.com>
2022-05-19 08:36:38 +0300

    Add files via upload

---
+++ 01.Basic Syntax, Conditional Statements and Loops/MonthPrinter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MonthPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 1 || num > 12)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            string[] month = { "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December" };
+
+            Console.WriteLine(month[num - 1]);
+        }
+    }
+}
