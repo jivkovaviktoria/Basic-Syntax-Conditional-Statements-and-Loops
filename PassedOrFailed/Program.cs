235bf12661fcb016310ea3e0447bd66f5c4d3600
Viktoriya Jivkova <85957657+jivkovaviktoria@users.noreply.github.com>
2022-05-19 08:31:34 +0300

    Add files via upload

--- PassedOrFailed/Program.cs
+++ PassedOrFailed/Program.cs
@@ -6,16 +6,7 @@ namespace PassedOrFailed
     {
         static void Main(string[] args)
         {
-            double grade = double.Parse(Console.ReadLine());
-
-            if (grade >= 3)
-            {
-                Console.WriteLine("Passed!");
-            }
-            else
-            {
-                Console.WriteLine("Failed!");
-            }
+           
         }
     }
 }
