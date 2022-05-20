6f13e5743d62ad183f965bb18687d0c886841653
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-20 09:48:22 +0300

    solved the "MultiplicationTable2.0" problem
    A program that receive the multiplier from the console. Prints the table with the multiplier in the interval from the given number to 10. If the given multiplier is more than 10, it prints only one row with the integer, the given multiplier, and the product.

---
+++ 01.Basic Syntax, Conditional Statements and Loops/MultiplicationTable/MultiplicationTable2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 10)
+            {
+                Console.WriteLine($"{n} X {a} = {n * a}");
+                return;
+            }
+            for (int i = a; i < 11; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}
