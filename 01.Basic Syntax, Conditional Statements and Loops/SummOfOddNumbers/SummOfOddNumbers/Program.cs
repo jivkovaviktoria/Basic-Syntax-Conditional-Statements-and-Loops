3833c771218dfff3557ce9e717384aa6dc9aa6ab
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-20 11:01:18 +0300

    Solved the "Sum of odd numbers" problem
    A program that prints on a new line the next n odd numbers (starting from 1). On the last row it prints the sum of all n odd numbers.

---
+++ 01.Basic Syntax, Conditional Statements and Loops/SummOfOddNumbers/SummOfOddNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOddNumbers = int.Parse(Console.ReadLine());
+            int counterOfOddNumbers = 0;
+            int sumOfOddNUmbers = 0;
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    sumOfOddNUmbers += i;
+                    counterOfOddNumbers++;
+                }
+                if (counterOfOddNumbers == numberOfOddNumbers)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Sum: {sumOfOddNUmbers}");
+        }
+    }
+}
