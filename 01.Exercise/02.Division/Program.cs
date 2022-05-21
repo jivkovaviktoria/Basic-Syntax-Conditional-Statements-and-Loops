3242a3a7ac0a7d54f8b65c953a8467f451f33128
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-21 11:32:34 +0300

    Solved task
    A program which checks if the given integer is divisible by 2 or 3, or 6, or 7, or 10 without a reminder. It always take the bigger division.

---
+++ 01.Exercise/02.Division/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02.Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            int[] divisible = new int[] { 2, 3, 6, 7, 10 };
+            int res = 0;
+
+            if (num % 2 == 0)
+            {
+                res = divisible[0];
+            }
+
+            if (num % 3 == 0)
+            {
+                res = divisible[1];
+            }
+
+            if (num % 6 == 0)
+            {
+                res = divisible[2];
+            }
+            if (num % 7 == 0)
+            {
+                res = divisible[3];
+            }
+            if (num % 10 == 0)
+            {
+                res = divisible[4];
+            }
+
+            if (num % 2 != 0 && num % 3 != 0 && num % 6 != 0 && num % 7 != 0 && num % 10 != 0)
+            {
+                Console.WriteLine("Not divisible");
+                return;
+            }
+
+            Console.WriteLine($"The number is divisible by {res}");
+        }
+    }
+}
