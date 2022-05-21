f7a4fec91906e69d0f545a185ca2719d77b012ce
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-21 11:37:42 +0300

    Solved task
    The program receives three lines from the console:
    
    · A count of people who are going on vacation.
    
    · Type of the group (Students, Business or Regular).
    
    · The day of the week which the group will stay on (Friday, Saturday or Sunday).
    
    Based on the given information the program will calculate how much the group will pay for the entire vacation

---
+++ 01.Exercise/Vacation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double people = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine().ToLower();
+            string day = Console.ReadLine().ToLower();
+            double price = 0;
+
+            if (day == "friday")
+            {
+                if (type == "students") price = 8.45;
+                else if (type == "business") price = 10.90;
+                else if (type == "regular") price = 15;
+            }
+
+            if (day == "saturday")
+            {
+                if (type == "students") price = 9.80;
+                else if (type == "business") price = 15.60;
+                else if (type == "regular") price = 20;
+            }
+
+            if (day == "sunday")
+            {
+                if (type == "students") price = 10.46;
+                else if (type == "business") price = 16;
+                else if (type == "regular") price = 22.50;
+            }
+
+            double total = people * price;
+            if (type == "students" && people >= 30) total = total - (total * 0.15);
+            else if (type == "business" && people >= 100) total = price * (people - 10);
+            else if (type == "regular" && people >= 10 && people <= 20) total = total - (total * 0.05);
+
+            Console.WriteLine($"Total price: {total:f2}");
+        }
+    }
+}
