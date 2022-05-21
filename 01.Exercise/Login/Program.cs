f58b9096fa8c69cb08a36ed757f0ea180a18c914
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-21 11:33:57 +0300

    Solved task
    "On the first line, you will be given a username and your task is to try to log in the user. The user's password is the username reversed. On the next lines, you will receive password"

---
+++ 01.Exercise/Login/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            int usernameLenght = username.Length - 1;
+            string password = "";
+
+            for (int i = usernameLenght; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            int countOfWrongPasswords = 0;
+            string inputPassword = Console.ReadLine();
+
+            while (inputPassword != password)
+            {
+                countOfWrongPasswords++;
+                if (countOfWrongPasswords > 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+                inputPassword = Console.ReadLine();
+            }
+
+            if (inputPassword == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
