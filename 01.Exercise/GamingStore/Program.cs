7213cbbac7175cf7d69297d52797e67dea23a52c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-22 16:43:58 +0300

    Solved task "Gaming store"

---
+++ 01.Exercise/GamingStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamingStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
