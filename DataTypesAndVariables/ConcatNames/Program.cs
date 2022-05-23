19231532b12e50086fa8fd1f797c755a10abf227
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-23 16:28:27 +0300

    Solved task
    A program that prints two names joined by the delimiter

---
+++ DataTypesAndVariables/ConcatNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string firstName = Console.ReadLine();
+            string secondtName = Console.ReadLine();
+
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName} {delimiter} {secondtName}");
+        }
+    }
+}
