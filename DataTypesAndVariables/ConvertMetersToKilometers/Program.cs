ef271ed210d52f2a5330eda9704fdd2d02354b46
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-23 10:24:41 +0300

    Solved task
    Meters to kilometers converter

---
+++ DataTypesAndVariables/ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double kilometers = meters * 0.001;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
