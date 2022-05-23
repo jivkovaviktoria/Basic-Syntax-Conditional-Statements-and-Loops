8587b2156b901706d43322114ae366392fd3f884
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-23 11:02:43 +0300

    Solved task
    Centuries to minutes converter

---
+++ DataTypesAndVariables/CenturiestToMinutes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CenturiestToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            double years = centuries * 100;
+            double days = Math.Floor(years * 365.2422);
+            double hours = days * 24;
+            double minutes = hours * 60;
+            Console.WriteLine(minutes);
+            
+        }
+    }
+}
