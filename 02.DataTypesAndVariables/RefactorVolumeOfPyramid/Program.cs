171fa3cc89746c342d6cc57a34dbef93b96cfa6d
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-24 14:35:26 +0300

    refactored code (Volume of pyramid)

--- 02.DataTypesAndVariables/RefactorVolumeOfPyramid/Program.cs
+++ 02.DataTypesAndVariables/RefactorVolumeOfPyramid/Program.cs
@@ -6,23 +6,22 @@ namespace RefactorVolumeOfPyramid
     {
         static void Main(string[] args)
         {
-            double dul, sh, V = 0;
-
+           
             Console.WriteLine("Length: ");
 
-            dul = double.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Width: ");
 
-            sh = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Heigth: ");
 
-            V = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
 
-            V = (dul + sh + V) / 3;
+            double Volume = (length + width + heigth) / 3;
 
-            Console.WriteLine($"Pyramid Volume: {V:f2}");
+            Console.WriteLine($"Pyramid Volume: {Volume:f2}");
         }
     }
 }
