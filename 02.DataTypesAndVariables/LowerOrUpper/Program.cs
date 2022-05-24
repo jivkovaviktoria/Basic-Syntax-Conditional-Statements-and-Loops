48b07824065bfe0d9cf2ec9d7666a9fae76ecaa5
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-24 14:26:50 +0300

    Solved taks (lower or upper)
    A program that prints whether a given character is upper-case or lower case.

---
+++ 02.DataTypesAndVariables/LowerOrUpper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+
+            if (Char.IsLower(firstChar))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else if (Char.IsUpper(firstChar))
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
