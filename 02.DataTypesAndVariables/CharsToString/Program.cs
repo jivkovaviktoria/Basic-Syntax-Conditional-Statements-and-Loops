75b79850ef928d5e51953ccdbb65c1de2f25f9b4
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-24 14:23:38 +0300

    Solved task (chars to string)
    A program that combines chars into string

---
+++ 02.DataTypesAndVariables/CharsToString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+
+            string charToString = firstChar.ToString() + secondChar.ToString() + thirdChar.ToString();
+            Console.WriteLine(charToString);
+                
+             
+        }
+    }
+}
