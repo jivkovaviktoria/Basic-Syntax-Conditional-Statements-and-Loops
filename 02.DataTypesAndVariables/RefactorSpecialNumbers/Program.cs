1de933949208cc45cb6b35328ffc6edc6d14a758
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-25 17:35:49 +0300

    Solved task "Refactor special numbers" from Data Types and Variables

---
+++ 02.DataTypesAndVariables/RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum= 0;
+            int number = 0;
+            bool result = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                number = i;
+
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+
+                result = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", number, result);
+                sum = 0;
+                i = number;
+            }
+        }
+    }
+}
