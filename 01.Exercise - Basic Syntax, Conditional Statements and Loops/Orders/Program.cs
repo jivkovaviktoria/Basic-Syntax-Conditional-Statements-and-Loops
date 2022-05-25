f0069208bf22099c6070291b912fb0f2972f615e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-25 16:49:03 +0300

    Solved task " Orders* " from Exercise: Basic Syntax, Conditional Statements and Loops

---
+++ 01.Exercise - Basic Syntax, Conditional Statements and Loops/Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            for (int i = 1; i <= orders; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double price = ((days * capsulesCount) * capsulePrice);
+                totalPrice += price;
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
