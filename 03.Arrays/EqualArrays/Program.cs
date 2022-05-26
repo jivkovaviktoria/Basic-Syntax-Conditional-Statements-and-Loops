45ecc1a90152e2451182272f9deaeecdde97a6fb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-26 12:05:56 +0300

    Solved task "Equal arrays" from Arrays-lab

---
+++ 03.Arrays/EqualArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                sum += firstArray[i];
+                count++;
+            }
+
+            if (firstArray.Length == count)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
