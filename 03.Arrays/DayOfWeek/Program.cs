22eb42368d93954a7e05844558f55ce5123c6458
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-26 11:58:06 +0300

    Solved task "Days of week" from Arrays-lab

---
+++ 03.Arrays/DayOfWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            string[] days = new string[] { "Invalid day!", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (day >= 1 && day <= 7)
+            {
+                Console.WriteLine(days[day]);
+            }
+            else
+            {
+                Console.WriteLine(days[0]);
+            }
+        }
+    }
+}
