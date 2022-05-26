4ca9eed3a4d2e9f9e9d44fe9e86c8c043924dc93
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-26 15:31:03 +0300

    Solved task "Reverse array of strings" from Array-lab

---
+++ 03.Arrays/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfStrings = Console.ReadLine().Split(" ").ToArray();
+
+            Array.Reverse(arrayOfStrings);
+
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+                Console.Write(arrayOfStrings[i] + " ");
+            }
+        }
+    }
+}
