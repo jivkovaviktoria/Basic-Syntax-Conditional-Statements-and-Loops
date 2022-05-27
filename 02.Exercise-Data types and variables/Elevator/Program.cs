37b1efe9b6cc6648a7d4d4000b68d73085409e78
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-27 23:37:54 +0300

    Solved task "Elevator" from Data types and variables-exercise

---
+++ 02.Exercise-Data types and variables/Elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+
+            int courses = people / capacity;
+
+            if (people % capacity != 0)
+            {
+                courses++;
+            }
+
+            Console.WriteLine(courses);
+        }
+    }
+}
