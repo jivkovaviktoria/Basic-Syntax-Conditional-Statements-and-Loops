4af276ee7c34fd9ab7952541f463014765d0a027
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-27 23:35:10 +0300

    Solved task "Integer operations" from Data types and variables-exercise

---
+++ 02.Exercise-Data types and variables/02.Exercise-Data types and variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02.Exercise_Data_types_and_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
