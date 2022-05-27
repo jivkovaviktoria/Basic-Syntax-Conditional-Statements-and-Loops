8b649366b314fb173d10d3214186820b6048bd0c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-27 23:40:29 +0300

    Solved task "Print Part Of ASCII Table" from Data types and variables-exercise

---
+++ 02.Exercise-Data types and variables/PrintPartOfASCIItable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintPartOfASCIItable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (char i = (char)start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
