4cb3f5a01c972f3fc766441787e8343abaea5b11
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-28 16:55:23 +0300

    Solved task "Train" from Arrays-exercise

---
+++ 03.Exercise - Arrays/03.Exercise - Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Exercise___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 03.Exercise - Arrays/Train/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] passengers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int numberOfPeople = int.Parse(Console.ReadLine());
+                passengers[i] = numberOfPeople;
+            }
+
+            foreach (int element in passengers)
+            {
+                Console.Write(element + " ");
+            }
+
+            int sum = passengers.Sum();
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
