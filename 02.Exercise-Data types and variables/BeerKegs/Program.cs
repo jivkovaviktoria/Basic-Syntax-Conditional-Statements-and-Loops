bd3f783db2aca0d6e054a97b616c5061bf9efdfd
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-28 11:04:07 +0300

    Solved task "Beer kegs" from Data types and variables-exercise

---
+++ 02.Exercise-Data types and variables/BeerKegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double volume = 0;
+            string biggestKeg = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double result = Math.PI * (radius * radius) * height;
+
+                if (result > volume)
+                {
+                    volume = result;
+                    biggestKeg = model;
+                }
+            }
+
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
