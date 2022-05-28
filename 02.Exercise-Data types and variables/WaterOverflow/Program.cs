52de69e6e1a14ce0e662f30bd57fb33501ac4986
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-28 10:44:50 +0300

    Solved task "Water overflow" from Data types and variables-exercise

---
+++ 02.Exercise-Data types and variables/WaterOverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (sum+quantity > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+                 sum += quantity;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
