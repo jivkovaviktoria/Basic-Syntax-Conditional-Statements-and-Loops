a9c6d6ed6526fb74a241fcb2486e700a4debf999
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-29 10:03:07 +0300

    Solved task " Poke mon* " from Data types and variables-exercise

---
+++ 02.Exercise-Data types and variables/PokeMon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            int temp = n;
+            while (temp >= m)
+            {
+                temp -= m;
+                counter++;
+
+                if (temp == n / 2 && y != 0)
+                {
+                    temp /= y;
+                }
+
+            }
+
+            Console.WriteLine(temp);
+            Console.WriteLine(counter);
+        }
+    }
+}
--- 02.Exercise-Data types and variables/SpiceMustFlow/Program.cs
+++ 02.Exercise-Data types and variables/SpiceMustFlow/Program.cs
@@ -6,7 +6,29 @@ namespace SpiceMustFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int startingYield = int.Parse(Console.ReadLine());
+            long total = 0;
+            int counter = 0;
+
+            while (startingYield >= 100)
+            {
+                total += startingYield;
+                startingYield -= 10;
+                total -= 26;
+                counter++;
+            }
+
+            if (total >= 26)
+            {
+                total -= 26;
+            }
+            else
+            {
+                total = 0;
+            }
+
+            Console.WriteLine(counter);
+            Console.WriteLine(total);
         }
     }
 }
