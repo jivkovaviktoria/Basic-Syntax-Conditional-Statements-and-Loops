88462eb32a3b9ec5778044a3bf60b44986ff10fa
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-30 11:18:52 +0300

    Solved task "Balanced brackets" from Data types and variables-More exercise

---
+++ 02.More exercise - Data types and variables/BalancedBrackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        bool isBalanced = true;
+        var stack = new Stack<char>();
+        var openedCount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            foreach (char character in input)
+            {
+                if (character == '(')
+                {
+                    openedCount++;
+                    if (openedCount == 2)
+                    {
+                        isBalanced = false;
+                    }
+                    stack.Push(character);
+                }
+                else if (character == ')')
+                {
+                    openedCount = 0;
+                    if (stack.Count() == 0)
+                        isBalanced = false;
+
+                    else if (stack.Pop() != '(')
+                        isBalanced = false;
+                }
+            }
+        }
+
+        if (stack.Count() != 0)
+        {
+            isBalanced = false;
+        }
+
+        Console.WriteLine(isBalanced ? "BALANCED" : "UNBALANCED");
+    }
+}
