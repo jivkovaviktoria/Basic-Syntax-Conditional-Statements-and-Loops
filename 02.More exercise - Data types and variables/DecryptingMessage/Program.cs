67a49c83ddfe193353616c39645f2dd89bf9696a
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-30 08:57:23 +0300

    Solved task "Decrypting message" from Data types and variables-More exercise

---
+++ 02.More exercise - Data types and variables/DecryptingMessage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DecryptingMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string word = "";
+            for (int i = 0; i < n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                word += (char)(letter + key);
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
