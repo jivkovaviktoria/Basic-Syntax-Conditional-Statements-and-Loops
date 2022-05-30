9ffb3d8211275865d92ead00924f2ae3fd8e0b45
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-30 09:07:36 +0300

    Solved task "Refactoring: Prime checker" from Data types and variables-More exercise

---
+++ 02.More exercise - Data types and variables/Refactoring-PrimeChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Refactoring_PrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int number = 2; number <= n; number++)
+            {
+                bool isPrime = true;
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", number, isPrime.ToString().ToLower());
+            }
+        }
+    }
+}
