6a1368b9f9261de6dcc99982f647e10c8ce0f750
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-30 08:56:15 +0300

    Solved task "From left to the right" from Data types and variables-More exercise

---
+++ 02.More exercise - Data types and variables/FromLeftToTheRight/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace FromLeftToTheRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                BigInteger sum = 0;
+                BigInteger[] numbers = Console.ReadLine().Split().Select(BigInteger.Parse).ToArray();
+
+                BigInteger biggerNumber = BigInteger.Abs(BigInteger.Max(numbers[0], numbers[1]));
+                while (biggerNumber != 0)
+                {
+                    sum += biggerNumber % 10;
+                    biggerNumber /= 10;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
