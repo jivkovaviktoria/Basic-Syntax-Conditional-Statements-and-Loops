11cc7ea9ad837ec92ad2722a9b7e06d7ee5429f0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-30 08:54:36 +0300

    Solved task "Data type finder" from Data types and variables-more exercise

---
+++ 02.More exercise - Data types and variables/DataTypeFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            while (text != "END")
+            {
+                if (int.TryParse(text, out _)) { Console.WriteLine($"{text} is integer type"); }
+                else if (double.TryParse(text, out _)) { Console.WriteLine($"{text} is floating point type"); }
+                else if (char.TryParse(text, out _)) { Console.WriteLine($"{text} is character type"); }
+                else if (bool.TryParse(text, out _)) { Console.WriteLine($"{text} is boolean type"); }
+                else { Console.WriteLine($"{text} is string type"); }
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
