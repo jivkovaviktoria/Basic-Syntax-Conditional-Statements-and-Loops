84a6a63c59fbcb78b133bc29984666233b8295bb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-05-31 10:32:13 +0300

    Solved task "Floating equality" from Data types and variables-More exercise

---
+++ 02.More exercise - Data types and variables/FloatingEquality/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double esp = 0.000001;
+            
+            bool equal = Math.Abs(firstNumber - secondNumber)<esp;
+
+            Console.WriteLine(equal);
+        }
+    }
+}
