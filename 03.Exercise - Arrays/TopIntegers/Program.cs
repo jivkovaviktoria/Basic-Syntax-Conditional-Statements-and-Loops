90caac8cf4b5f871afa30186a3e468a41636c29a
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-03 20:20:01 +0300

    Solved task "Top integers" from Arrays-exercise

---
+++ 03.Exercise - Arrays/TopIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTopNumber = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] >= numbers[i])
+                    {
+                        isTopNumber = false;
+                        break;
+                    }
+                }
+                if (isTopNumber)
+                    result.Add(numbers[i]);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
