4226bac76a9e1f60d21b98c81613cc5727d3f027
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-03 20:39:42 +0300

    Solved task "Equal sums" from arrays-exercise

---
+++ 03.Exercise - Arrays/EqualSums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int k = 0; k < i; k++)
+                {
+                    leftSum += numbers[k];
+                }
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+        }
+    }
+}
