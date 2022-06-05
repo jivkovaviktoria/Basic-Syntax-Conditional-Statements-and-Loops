eaa720444ed954551e23df9660bc67dccf5b3050
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-05 18:52:56 +0300

    Solved task "SIgn of integer numbers" from Methods-lab

---
+++ 04.Methods/SIgnOfIntegerNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SIgnOfIntegerNumbers
+{
+    class Program
+    {
+        static void SignOfNumber(int number)
+        {
+            string sign = "";
+            if (number < 0) Console.WriteLine($"The number {number} is negative.");
+            else if(number>0) Console.WriteLine($"The number {number} is positive."); 
+            else if(number == 0) Console.WriteLine($"The number {number} is zero.");
+            
+        }
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            SignOfNumber(number);
+        }
+    }
+}
