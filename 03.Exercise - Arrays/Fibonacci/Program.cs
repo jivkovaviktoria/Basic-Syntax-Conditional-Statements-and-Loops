3a5a3200f4935df7b238e175454b11059d204f31
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-06 20:39:27 +0300

    Solved task "Fibonacci" from arrays-more-exercise

---
+++ 03.Exercise - Arrays/Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] elements = new int[n];
+            elements[0] = 1;
+
+            if (n > 1)
+                elements[1] = 1;
+
+            for (int i = 2; i < elements.Length; i++)
+            {
+                elements[i] = elements[i - 1] + elements[i - 2];
+            }
+            Console.WriteLine(elements[n - 1]);
+        }
+    }
+}
