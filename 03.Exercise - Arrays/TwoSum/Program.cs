3e5a1eeb59a0828ef7d1b88c097c1f1b5b16026b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-06 21:47:43 +0300

    Solved the "Two sum" task.

---
+++ 03.Exercise - Arrays/TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int targetSum = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNumber = nums[i];
+                if (dict.ContainsKey(currentNumber))
+                {
+                    Console.WriteLine(dict[currentNumber]);
+                    Console.WriteLine(i);
+                    break;
+                }
+
+                dict[targetSum - currentNumber] = i;
+            }
+        }
+    }
+}
