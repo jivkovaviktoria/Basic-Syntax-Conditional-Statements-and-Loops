1a51c19a831ec4bd942fa1ec64a6b63724de8bde
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-07 15:27:57 +0300

    Solved task "Fold and sum" from Arrays-more exercise

---
+++ 03.Exercise - Arrays/FoldAndSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mainArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int k = mainArray.Length / 4;
+            int[] foldedArray = new int[k * 2];
+
+            for (int i = 0; i < foldedArray.Length / 2; i++)
+                foldedArray[i] = mainArray[k - (i + 1)];
+
+            int a = 0;
+            for (int i = foldedArray.Length / 2; i < foldedArray.Length; i++)
+            {
+                a++;
+                foldedArray[i] = mainArray[mainArray.Length - a];
+            }
+
+            int[] result = new int[foldedArray.Length];
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = foldedArray[i] + mainArray[i + k];
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
