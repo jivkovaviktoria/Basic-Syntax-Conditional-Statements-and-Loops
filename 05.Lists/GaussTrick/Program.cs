cbcb3881be1ccc01d3aa86477c149dc8cc927217
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-08 19:43:30 +0300

    Solved task "Gauss trick" from Lists-lab

---
+++ 05.Lists/GaussTrick/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int halfLength = list.Count / 2;
+            for (int i = 0; i < halfLength; i++)
+            {
+                list[i] = list[i] + list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
