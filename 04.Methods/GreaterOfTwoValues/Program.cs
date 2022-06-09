100ae930a1c07dc73d923848c48db842de0a6a52
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-09 22:13:59 +0300

    Solved task "Greater of two values" from Methods-lab

---
+++ 04.Methods/GreaterOfTwoValues/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if (type == "int")
+            {
+                int firstNumber = int.Parse(Console.ReadLine());
+                int secondNumber = int.Parse(Console.ReadLine());
+                GetGreater(firstNumber, secondNumber);
+            }
+            else if (type == "char")
+            {
+                char firstChar = char.Parse(Console.ReadLine());
+                char secondChar = char.Parse(Console.ReadLine());
+                GetGreater(firstChar, secondChar);
+            }
+            else if (type == "string")
+            {
+                string firstString = Console.ReadLine();
+                string secondString = Console.ReadLine();
+                GetGreater(firstString, secondString);
+            }
+        }
+
+        static int GetGreater(int firstNumber, int secondNumber)
+        {
+            return firstNumber > secondNumber ? firstNumber : secondNumber;
+        }
+
+        static char GetGreater(char firstChar, char secondChar)
+        {
+            return firstChar > secondChar ? firstChar : secondChar;
+        }
+
+        static string GetGreater(string firstString, string secondString)
+        {
+            int result = firstString.CompareTo(secondString);
+            if (result > 0)
+            {
+                return firstString;
+            }
+            else
+            {
+                return secondString;
+            }
+        }
+    }
+}
