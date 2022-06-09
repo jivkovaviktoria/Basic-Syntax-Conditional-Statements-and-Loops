582a09d527b16e135a64666f033c602cc67114c4
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-09 22:26:35 +0300

    Solved task "Multiply even by odds" from Methods-lab

---
+++ 04.Methods/MultiplyEvenByOdds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MultiplyEvenByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = Calculate(number);
+            Console.WriteLine(result);
+
+        }
+
+        static int Calculate(int num)
+        {
+            int evenSum = 0;
+            int oddSum = 0;
+
+            while (num != 0)
+            {
+                int lastDigit = num % 10;
+
+                if (lastDigit%2==0)
+                {
+                    evenSum += lastDigit;
+                }
+                else
+                {
+                    oddSum += lastDigit;
+                }
+                num /= 10;
+            }
+
+            int result = evenSum * oddSum;
+            return result;
+
+        }
+    }
+}
