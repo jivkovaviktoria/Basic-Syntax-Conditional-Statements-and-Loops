534272e234f3f11344459d2ffaad7db4fca1085d
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-10 22:28:59 +0300

    Solved task "Smallest of three numbers"

---
+++ 04.Methods/SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(smallestNum(firstNumber, secondNumber, thirdNumber));
+        }
+        static int smallestNum(int first, int second, int third)
+        {
+            int smallest = first;
+            if (second<smallest)
+                smallest = second;
+            if (third < smallest)
+                smallest = third;
+
+            return smallest;
+        }
+
+            
+    }
+}
