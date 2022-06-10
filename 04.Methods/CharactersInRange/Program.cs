d71573852430d41f83e52c80856fc6170356afec
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-10 22:47:50 +0300

    SOlved task "Characters in range" from Methods-exercise

---
+++ 04.Methods/CharactersInRange/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                char inputOne = char.Parse(Console.ReadLine());
+                char inputTwo = char.Parse(Console.ReadLine());
+
+                PrintTheCharsBetweenTwoChars(inputOne, inputTwo);
+            
+
+            static void PrintTheCharsBetweenTwoChars(char firstCharacter, char secondCharacter)
+            {
+                int startCharacter = Math.Min(firstCharacter, secondCharacter);
+                int endCharacter = Math.Max(firstCharacter, secondCharacter);
+
+                for (int i = ++startCharacter; i < endCharacter; i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
