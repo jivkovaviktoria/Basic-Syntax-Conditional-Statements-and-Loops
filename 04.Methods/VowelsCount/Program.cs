95d7d8c7f18e305c3beae36ca3d88d937de4cee0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-11 23:37:17 +0300

    Solved task "Vowels count" from Methods-exercise

---
+++ 04.Methods/VowelsCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(VowelsCount(word));
+        }
+
+        static int VowelsCount(string word)
+        {
+
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            word = word.ToLower();
+            int vowelsCount = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (vowels.Contains(word[i]))
+                    vowelsCount++;
+            }
+            return vowelsCount;
+        }
+    }
+}
