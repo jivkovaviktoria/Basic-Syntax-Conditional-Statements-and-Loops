69977a2817c14340dc78d7a4052016dd4cdbbf72
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-11 23:55:55 +0300

    Solved task "Add and subtract" from Methods-exercise

---
+++ 04.Methods/AddAndSubtrackt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int result = Sum(firstNum, secondNum);
+            int total = Subtract(thirdNum, result);
+            Console.WriteLine(total);
+        }
+        static int Sum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        static int Subtract(int thirdNum, int result)
+        {
+            return result - thirdNum;
+        }
+    }
+}
