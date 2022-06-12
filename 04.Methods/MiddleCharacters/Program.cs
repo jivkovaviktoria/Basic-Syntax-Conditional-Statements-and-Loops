e2109fd6d54dc4b64398817f3791df9b76d96700
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-12 23:01:18 +0300

    Solved task "Middle characters" from Methods-exercise

---
+++ 04.Methods/MiddleCharacters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string result = MiddleCharacters(text);
+            Console.WriteLine(result);
+        }
+        static string MiddleCharacters(string text)
+        {
+            int middle = text.Length/2;
+            if (text.Length%2==0)
+            {
+                return $"{text[middle-1]}{text[middle]}";
+            }
+            else
+            {
+                return $"{text[middle]}";
+            }
+        }
+    }
+    
+
+}
