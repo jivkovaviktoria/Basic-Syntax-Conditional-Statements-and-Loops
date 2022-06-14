e2ac4f941ed56327276190e88b9c17499e65b490
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-14 17:23:53 +0300

    Solved task "NxN Matrix" from Methods-exercise

---
+++ 04.Methods/NxN-Matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Matrix(n);
+        }
+
+        static void Matrix(int n)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
