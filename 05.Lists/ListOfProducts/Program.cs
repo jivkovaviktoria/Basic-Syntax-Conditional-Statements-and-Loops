bdbadcb4150dea0e406ed5edd3f22967493b80e3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-15 20:25:06 +0300

    Solved task "List of products" from Lists-lab

---
+++ 05.Lists/ListOfProducts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOfProducts
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+
+            int counter = 1;
+            foreach(var product in products)
+            {
+                Console.WriteLine($"{counter}.{product}");
+                counter++;
+            }
+        }
+    }
+}
