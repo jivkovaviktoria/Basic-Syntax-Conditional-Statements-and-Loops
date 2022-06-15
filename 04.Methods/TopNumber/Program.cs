b6ed039bdf7fc6f0bdf43761edb9299e1064d77a
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-15 12:59:05 +0300

    Solved task "Top number" from Methods-exercise

---
+++ 04.Methods/TopNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            TopNumbers(n);
+        }
+        static void TopNumbers(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                bool hasOddDigits = false;
+                bool isDividibleByEight = false;
+                int lastDigit = 0;
+                int sumOfDigits = 0;
+                int number = i;
+
+                while(number!=0)
+                {
+                    lastDigit = number % 10;
+                    if (lastDigit % 2 != 0)
+                        hasOddDigits = true;
+
+                    sumOfDigits += lastDigit;
+                    number /= 10;
+                }
+                if (sumOfDigits % 8 == 0)
+                    isDividibleByEight = true;
+
+                if (hasOddDigits && isDividibleByEight)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
