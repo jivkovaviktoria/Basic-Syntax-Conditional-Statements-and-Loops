e138c8b9d88ad26c1044a4e04bc0bd0ed5363c21
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-19 15:09:41 +0300

    Solved task "Bonus scoring system" from Mid exam-exercise

---
+++ MidExams-exercise/BonusScoringSystem/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BonusScoringSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = double.Parse(Console.ReadLine());
+            var lectures = double.Parse(Console.ReadLine());
+
+            var bonus = double.Parse(Console.ReadLine());
+
+            var maxBonus = int.MinValue;
+            int maxAtt = int.MinValue;
+
+
+            if (students == 0 || lectures == 0)
+            {
+                Console.WriteLine($"Max Bonus: 0.");
+                Console.WriteLine($"The student has attended 0 lectures.");
+                return;
+            }
+
+            for (int i = 0; i < students; i++)
+            {
+                int attendance = int.Parse(Console.ReadLine());
+
+                double temp = Math.Ceiling(attendance / lectures * (5 + bonus));
+
+                if (temp > maxBonus)
+                {
+                    maxBonus = (int)temp;
+                    maxAtt = attendance;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {maxBonus}.");
+            Console.WriteLine($"The student has attended {maxAtt} lectures.");
+        }
+    }
+}
