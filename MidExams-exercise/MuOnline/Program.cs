e8f433cb0b227e29ad45310a1f6916f5676a3108
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-20 12:14:27 +0300

    Solved task "Mu online" from Mid-exam-exercise

--- MidExams-exercise/MuOnline/Program.cs
+++ MidExams-exercise/MuOnline/Program.cs
@@ -7,56 +7,75 @@ namespace MuOnline
     {
         static void Main(string[] args)
         {
-            string[] rooms = Console.ReadLine().Split('|', ' ').ToArray();
             int initialHealth = 100;
-            int initialBitcoins = 0;
+            int counterRooms = 0;
+            int bitcoins = 0;
 
-            for (int i = 0; i < rooms.Length; i += 2)
+            string[] dungeousRooms = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+            string[] currentRoom = new string[dungeousRooms.Length];
+            for (int i = 0; i < dungeousRooms.Length; i++)
             {
-                if (rooms[i] == "potion")
+                currentRoom = dungeousRooms[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = currentRoom[0];
+
+                int numberFromInput = int.Parse(currentRoom[1]);
+
+
+                switch (command)
                 {
-                    int amout = int.Parse(rooms[i + 1]);
+                    case "potion":
+                        if (initialHealth == 100)
+                        {
+                            counterRooms++;
+                            continue;
+                        }
+                        else if (initialHealth < 100)
+                        {
+                            bool isEnoughHealing = numberFromInput + initialHealth > 100;
+                            if (isEnoughHealing == false)
+                            {
+                                int healthPoint = numberFromInput;
+                                initialHealth += numberFromInput;
+                                Console.WriteLine($"You healed for {healthPoint} hp.");
+                                Console.WriteLine($"Current health: {initialHealth} hp.");
+                                counterRooms++;
+                            }
+                            else
+                            {
+                                int difference = 100 - initialHealth;
+                                initialHealth += difference;
+                                Console.WriteLine($"You healed for {difference} hp.");
+                                Console.WriteLine($"Current health: {initialHealth} hp.");
+                                counterRooms++;
+                            }
+                        }
+                        break;
+                    case "chest":
+                        int currentBitcoin = numberFromInput;
+                        Console.WriteLine($"You found {numberFromInput} bitcoins.");
+                        bitcoins += currentBitcoin;
+                        counterRooms++;
+                        break;
+                    default:
+                        bool isHealthZero = initialHealth - numberFromInput <= 0;
 
-                    if (initialHealth+amout > 100)
-                    {
-                        amout = 100 - initialHealth;
-                        initialHealth = 100;
-                    }
-                    else
-                    {
-                        initialHealth += amout;
-                    }
-                    Console.WriteLine($"You healed for {amout} hp.");
-                    Console.WriteLine($"Current health: {initialHealth} hp.");
-
-                }
-                else if (rooms[i] == "chest")
-                {
-                    int bitcoins = int.Parse(rooms[i + 1]);
-                    Console.WriteLine($"You found {bitcoins} bitcoins.");
-                    initialBitcoins += bitcoins;
-
-                }
-                else if (rooms[i] != "potion" || rooms[i] != "chest")
-                {
-                    int attack = int.Parse(rooms[i + 1]);
-                    string monster = rooms[i];
-
-                    initialHealth -= attack;
-
-                    if (initialHealth > 0)
+                        if (isHealthZero == true)
+                        {
+                            counterRooms++;
+                            Console.WriteLine($"You died! Killed by {command}.");
+                            Console.WriteLine($"Best room: {counterRooms}");
+                            return;
+                        }
+                        string monster = command;
+                        int monsterDamage = numberFromInput;
                         Console.WriteLine($"You slayed {monster}.");
-                    else
-                    {
-                        Console.WriteLine($"You died! Killed by {monster}.");
-                        Console.WriteLine($"Best room: {i-4}");
-                        return;
-                    }
+                        initialHealth -= monsterDamage;
+                        counterRooms++;
+                        break;
                 }
             }
-
-            Console.WriteLine($"You've made it!");
-            Console.WriteLine($"Bitcoins: {initialBitcoins}");
+            Console.WriteLine("You've made it!");
+            Console.WriteLine($"Bitcoins: {bitcoins}");
             Console.WriteLine($"Health: {initialHealth}");
         }
     }
