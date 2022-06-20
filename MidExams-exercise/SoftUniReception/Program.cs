45523072a96e6e63a8492f59d2cf9329d4668e8c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-20 14:10:14 +0300

    Solved task "SoftUni reception" from Mid exam-exercise

---
+++ MidExams-exercise/SoftUniReception/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+
+            int students = int.Parse(Console.ReadLine());
+
+            int studentsPerHour = firstEmployee + secondEmployee + thirdEmployee;
+            int studentsLeft = students;
+            int result = 0;
+            int countHours = 1;
+            while (studentsLeft > 0)
+            {
+                if(countHours % 4 == 0)
+                {
+                    result++;
+                    countHours++;
+                    continue;
+                }
+
+                studentsLeft -= studentsPerHour;
+                result++;
+                countHours++;
+            }
+
+            Console.WriteLine($"Time needed: {result}h.");
+        }
+    }
+}
