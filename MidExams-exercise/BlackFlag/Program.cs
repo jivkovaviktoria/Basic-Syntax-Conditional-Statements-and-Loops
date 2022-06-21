4943fbb06984a36c4f8223624558c0d7970d19c3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-21 09:10:28 +0300

    Solved task "Black flag" from Mid exam preparation

---
+++ MidExams-exercise/BlackFlag/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BlackFlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double gainedPlunder = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                gainedPlunder += dailyPlunder;
+
+                if (i % 3 == 0)
+                    gainedPlunder += dailyPlunder * 0.5;
+
+                if (i % 5 == 0)
+                    gainedPlunder -= gainedPlunder * 0.3;
+            }
+
+            if (gainedPlunder >= expectedPlunder)
+                Console.WriteLine($"Ahoy! {gainedPlunder:f2} plunder gained.");
+            else
+            {
+                double percentage = gainedPlunder / expectedPlunder * 100;
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+
+        }
+    }
+}
