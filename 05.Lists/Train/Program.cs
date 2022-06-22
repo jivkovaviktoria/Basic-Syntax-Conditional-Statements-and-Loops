05fbaffb71a66fcc61888a6d8bd947eb07b10b1e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-22 10:16:14 +0300

    Solved task "Train" from Lists-exercise

---
+++ 05.Lists/Train/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while(input!="end")
+            {
+                string[] tokens = input.Split();
+
+                if(tokens[0]=="Add")
+                {
+                    int passengers = int.Parse(tokens[1]);
+                    wagons.Add(passengers);
+                }
+                else
+                {
+                    int passengers = int.Parse(tokens[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if(wagons[i]+passengers <= capacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
