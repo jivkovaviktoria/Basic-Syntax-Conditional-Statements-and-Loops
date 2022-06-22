c4267847344f7d0f3ecde8dab114d15f288e3449
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-22 10:29:30 +0300

    Solved task "Change list" from Lists-exercise

---
+++ 05.Lists/ChangeList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string input = Console.ReadLine();
+            
+
+            while (input!="end")
+            {
+
+                string[] tokens = input.Split();
+                string command = tokens[0];
+
+                if (command == "Delete")
+                {
+                    int element = int.Parse(tokens[1]);
+                    integers.RemoveAll(x => x == element);
+                }
+                else if(command == "Insert")
+                {
+                    int element = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+
+                    integers.Insert(index, element);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
