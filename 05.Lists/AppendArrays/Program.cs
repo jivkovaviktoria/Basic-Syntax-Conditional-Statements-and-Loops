eb04f3e590d36e3a7d4275da28f982707faefe70
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-25 09:54:51 +0300

    Solved task "Append arrays" from Lists-exercise

---
+++ 05.Lists/AppendArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] numbers = Console.ReadLine().Split('|').ToArray();
+
+            List<string> result = new List<string>();
+
+
+            for (int i = numbers.Length-1; i >= 0; i--)
+            {
+                string elements = numbers[i];
+                string[] arr = elements.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    result.Add(arr[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
