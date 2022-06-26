15bb1b4a2d25454a9b09be833d94b9778640c7f6
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-26 19:10:21 +0300

    Solved task "Messaging" from Lists-More exercises

---
+++ 05.Lists/Messaging/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<char> text = Console.ReadLine().ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int index = GetIndex(numbers[i]);
+
+                if(index>text.Count-1)
+                {
+                    while (index > text.Count)
+                        index -= text.Count;
+                }
+
+                Console.Write(text[index]);
+                text.RemoveAt(index);
+            }
+        }
+
+        static int GetIndex(int num)
+        {
+            int index = 0;
+            while (num!=0)
+            {
+                index += num % 10;
+                num /= 10;
+            }
+
+            return index;
+        }
+    }
+}
