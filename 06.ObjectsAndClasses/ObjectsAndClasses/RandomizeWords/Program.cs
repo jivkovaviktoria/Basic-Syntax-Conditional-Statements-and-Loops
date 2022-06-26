d8d83b4c5f1755e83da2d161965c1fab174f6010
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-26 20:51:52 +0300

    Solved task "Randomize words" from Objects and classes - lab

---
+++ 06.ObjectsAndClasses/ObjectsAndClasses/RandomizeWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            Random random = new Random();
+            int index = random.Next(0, words.Length);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                words[i] = words[index];
+                words[index] = currentWord;
+            }
+
+            foreach (var word in words)
+                Console.WriteLine(word);
+
+        }
+    }
+}
