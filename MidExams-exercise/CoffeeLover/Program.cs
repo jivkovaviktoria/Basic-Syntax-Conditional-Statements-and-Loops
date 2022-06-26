1c6328abd29e8e131e55e31c2ea8dc18c50d89ff
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-26 19:19:50 +0300

    Solved task "Coffee lover" from Mid exam

---
+++ MidExams-exercise/CoffeeLover/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> coffees = Console.ReadLine().Split().ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split();
+                string command = tokens[0];
+
+                if (command == "Include")
+                    coffees.Add(tokens[1]);
+
+
+                else if (command == "Reverse")
+                    coffees.Reverse();
+
+                else if (command == "Prefer")
+                {
+                    int index1 = int.Parse(tokens[1]);
+                    int index2 = int.Parse(tokens[2]);
+                    if (index1 >= 0 && index1 <= coffees.Count - 1 && index2 >= 0 && index2 <= coffees.Count - 1)
+                    {
+                        string tmp = coffees[index1];
+                        coffees[index1] = coffees[index2];
+                        coffees[index2] = tmp;
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    string firstOrLast = tokens[1];
+                    int count = int.Parse(tokens[2]);
+
+                    if (count <= coffees.Count)
+                    {
+                        for (int j = 0; j < count; j++)
+                        {
+                            if (firstOrLast == "first") coffees.RemoveAt(0);
+                            else if (firstOrLast == "last") coffees.RemoveAt(coffees.Count - 1);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Coffees:");
+            Console.WriteLine(string.Join(" ", coffees));
+        }
+    }
+}
