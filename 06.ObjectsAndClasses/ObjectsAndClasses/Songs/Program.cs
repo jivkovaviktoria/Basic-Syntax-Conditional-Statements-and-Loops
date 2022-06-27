59e7682d5d18055f0d15f172ef4d31038e326bfa
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-27 12:19:49 +0300

    Solved task "Songs" from Objects and classes - lab

---
+++ 06.ObjectsAndClasses/ObjectsAndClasses/Songs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Songs
+{
+    class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < countOfSongs; i++)
+            {
+                string[] tokens = Console.ReadLine().Split('_', StringSplitOptions.RemoveEmptyEntries);
+                string typeList = tokens[0];
+                string name = tokens[1];
+                string time = tokens[2];
+
+                Song newSong = new Song()
+                {
+                    Name = name,
+                    TypeList = typeList,
+                    Time = time,
+                };
+
+                songs.Add(newSong);
+            }
+
+            string type = Console.ReadLine();
+
+            if (type == "all")
+            {
+                foreach (var song in songs)
+                    Console.WriteLine(song.Name);
+            }
+            else
+            {
+                List<Song> filteredSongs = songs.FindAll(song => song.TypeList == type);
+                foreach (var song in filteredSongs)
+                    Console.WriteLine(song.Name);
+            }
+        }
+    }
+}
