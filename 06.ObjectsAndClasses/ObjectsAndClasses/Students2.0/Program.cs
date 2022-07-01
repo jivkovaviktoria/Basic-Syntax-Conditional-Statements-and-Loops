8988c373ca3377b3ada3e43689e3bd15869850d1
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-01 19:43:18 +0300

    Solved task "Students" from Objects and classes - exercise

---
+++ 06.ObjectsAndClasses/ObjectsAndClasses/Students2.0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split();
+
+                Student student = new Student(studentInfo[0], studentInfo[1], double.Parse(studentInfo[2]));
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(currStudent => currStudent.Grade).ToList();
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+    }
+}
