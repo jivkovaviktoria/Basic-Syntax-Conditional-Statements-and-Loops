10fb260ee0ecb8882d78a32ebb0abc24d58c641f
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-07 16:56:35 +0300

    Solved task "Odd occurrences" from Associative arrays - lab

---
+++ 07.AssociativeArrays/OddOccurrences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Select(w => w.ToLower()).ToArray();
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!occurrences.ContainsKey(word))
+                    occurrences.Add(word, 1);
+                else
+                    occurrences[word]++;
+            }
+
+            foreach (var item in occurrences)
+            {
+                if (item.Value % 2 != 0)
+                    Console.Write($"{item.Key} ");
+            }
+        }
+    }
+}
