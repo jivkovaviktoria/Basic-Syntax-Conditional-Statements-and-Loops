0a2d2adcad3fd29b182bc012b10a00b36dca967b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-07 17:40:02 +0300

    Solved task "Count chars in a string" from Associative arrays - exercise

---
+++ 07.AssociativeArrays/CountCharsInAString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            char[] text = input.ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray();
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (var letter in text)
+            {
+                if (!counts.ContainsKey(letter))
+                    counts.Add(letter, 1);
+                else
+                    counts[letter]++;
+            }
+
+            foreach (var item in counts)
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
