32f61d2a66d25e6c4a81bfe653b1e7d4514b3bcc
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-07 16:43:10 +0300

    Solved task "Count real numbers" from Associative arrays - lab

---
+++ 07.AssociativeArrays/CountRealNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!counts.ContainsKey(number))
+                    counts.Add(number, 1);
+                else
+                    counts[number] += 1;
+            }
+
+            foreach (var item in counts)
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
