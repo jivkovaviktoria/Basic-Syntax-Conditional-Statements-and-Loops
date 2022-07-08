23e3a84a0747a64aca48f2c4e93155df028ae397
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-08 10:51:48 +0300

    Solved task "SoftUni parking" from Associative arrays - exercise

---
+++ 07.AssociativeArrays/SoftUniParking/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> users = new Dictionary<string, string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string username = tokens[1];
+
+                if (command == "register")
+                {
+                    string plate = tokens[2];
+
+                    if (!users.ContainsKey(username))
+                    {
+                        users.Add(username, plate);
+                        Console.WriteLine($"{username} registered {plate} successfully");
+                    }
+                    else
+                        Console.WriteLine($"ERROR: already registered with plate number {plate}");
+                }
+                else if(command=="unregister")
+                {
+                    if(users.ContainsKey(username))
+                    {
+                        users.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else
+                        Console.WriteLine($"ERROR: user {username} not found");
+                }
+            }
+
+            foreach (var (username, plate) in users)
+                Console.WriteLine($"{username} => {plate}");
+        }
+    }
+}
