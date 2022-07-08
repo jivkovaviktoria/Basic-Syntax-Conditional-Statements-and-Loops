041a024a5bcfa465e5f37644b2e4801798cb9d59
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-08 11:01:20 +0300

    Solved task "Courses" from Associative arrays - exercise

---
+++ 07.AssociativeArrays/Courses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(" : ");
+                string courseName = tokens[0];
+                string student = tokens[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                    courses[courseName].Add(student);
+                }
+                else
+                    courses[courseName].Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+
+                foreach (var item in course.Value)
+                    Console.WriteLine($"-- {item}");
+            }
+
+        }
+    }
+}
