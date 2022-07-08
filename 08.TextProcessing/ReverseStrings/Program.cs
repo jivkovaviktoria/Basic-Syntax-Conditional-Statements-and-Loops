abb568553c6442a70c8810d0229ba90933fe6636
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-08 18:31:48 +0300

    Solved task "Reverse strings" from Text processing - lab

---
+++ 08.TextProcessing/ReverseStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            while (text!="end")
+            {
+                Console.WriteLine($"{text} = {Reverse(text)}");
+                text = Console.ReadLine();
+            }
+        }
+        static string Reverse(string text)
+        {
+            string reversedText = "";
+
+            for (int i = text.Length-1; i >= 0; i--)
+                reversedText += text[i];
+
+            return reversedText;
+        }
+    }
+    
+}
