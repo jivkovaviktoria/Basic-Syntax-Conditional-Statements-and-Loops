6e2cacf9dd81a29896c34dce7332e3d658ef2367
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-08 11:25:30 +0300

    Solved task "Company users" from Associative arrays - exercise

---
+++ 07.AssociativeArrays/CompanyUsers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<String>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split(" -> ");
+                string company = tokens[0];
+                string id = tokens[1];
+
+                if (!companies.ContainsKey(company))
+                {
+                    companies.Add(company, new List<string>());
+                    companies[company].Add(id);
+                }
+                else
+                {
+                    bool containsId = companies[company].Contains(id);
+
+                    if (!containsId)
+                        companies[company].Add(id);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+
+                foreach (var id in company.Value)
+                    Console.WriteLine("-- " + id);
+            }
+
+        }
+    }
+}
