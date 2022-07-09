076cfcff7bc08ed19675b05c9bd7eb1241680f6e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-09 12:13:48 +0300

    Solved task "Repeat strings" from Text processing - lab

---
+++ 08.TextProcessing/RepeatStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in text)
+            {
+                for (int i = 0; i < word.Length; i++)
+                    sb.Append(word);
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
