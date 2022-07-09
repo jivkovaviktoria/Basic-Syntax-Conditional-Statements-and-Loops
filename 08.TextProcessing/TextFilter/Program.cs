cf6ef3f94a17ddcfdeaa911aef1adc6e16d51849
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-09 12:36:28 +0300

    Solved task "Text filter" from Text processing - lab

---
+++ 08.TextProcessing/TextFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWord = Console.ReadLine().Split(new char[] { ' ', ',', '.', '!', '*' }, StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var word in banWord)
+                text = text.Replace(word, new string('*', word.Length));
+
+            Console.WriteLine(text);
+        }
+
+
+    }
+}
