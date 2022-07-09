64fbd491788bcfab7a8a13e6d2e0655945d9ee9b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-09 13:50:00 +0300

    Solved task "Extract file" from Text processing - exercise

---
+++ 08.TextProcessing/ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] extract = Console.ReadLine().Split(':', '\\').ToArray();
+            string[] file = extract.Last().Split('.').ToArray();
+
+            string fileName = file[0];
+            string extension = file[1];
+
+            Console.WriteLine($"File name: {fileName}\nFile extension: {extension}");
+        }
+    }
+}
