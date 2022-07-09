8281df2973f1fdeb364bea1dd02b472bda26dbf3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-09 12:55:10 +0300

    Solved task "Digits, letters and others" from Text processing - lab

---
+++ 08.TextProcessing/DigitsLettersAndOthers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace DigitsLettersAndOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var letters = new StringBuilder();
+            var digits = new StringBuilder();
+            var others = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentSymbol = input[i];
+
+                if (char.IsLetter(currentSymbol))
+                    letters.Append(currentSymbol);
+                else if (char.IsDigit(currentSymbol))
+                    digits.Append(currentSymbol);
+                else
+                    others.Append(currentSymbol);
+            }
+
+            Console.WriteLine($"{digits}\n{letters}\n{others}");
+        }
+    }
+}
