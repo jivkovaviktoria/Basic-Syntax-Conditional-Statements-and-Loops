4cec46e4baf3d981c09fc34be8b6c257063ecf2f
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-10 11:44:17 +0300

    Solved task "Match full name" from Regular expressions - lab

---
+++ 09.RegularExpressions/MatchFullName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            string names = Console.ReadLine();
+            MatchCollection mathedNames = Regex.Matches(names, regex);
+
+            foreach (Match match in mathedNames)
+                Console.Write(match.Value + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
