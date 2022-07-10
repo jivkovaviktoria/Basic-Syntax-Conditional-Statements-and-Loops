0aa9b20ddc942b215d97e0e2ea10cf7e1e2185b0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-10 12:11:03 +0300

    Solved task "Match phone numbers" from Regular expressions - lab

---
+++ 09.RegularExpressions/MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+            string phones = Console.ReadLine();
+
+            MatchCollection phoneMatches = Regex.Matches(phones, regex);
+
+            string[] matchPhones = phoneMatches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", matchPhones));
+        }
+    }
+}
