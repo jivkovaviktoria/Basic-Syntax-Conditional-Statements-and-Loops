816a3978fa70b0c4050e3775c2c498d0a2f5f6af
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-12 17:22:52 +0300

    Solved task "Destination mapper" from final exam - exercise

---
+++ FinalExam-exercise/DestinationMapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DestinationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"(=|\/)(?<place>[A-Z][A-za-z]{2,})\1";
+            List<string> places = new List<string>();
+
+            string text = Console.ReadLine();
+
+            MatchCollection destinations = Regex.Matches(text, pattern);
+            var travelPoints = 0;
+
+            foreach (Match destination in destinations)
+            {
+                places.Add(destination.Groups["place"].Value);
+                travelPoints += destination.Groups["place"].Value.Length;
+            }
+
+            Console.WriteLine($"Destinations: { string.Join(", ", places)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
