171c1a1e5e3c8f8fd6c6413969536d558ab69aa7
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-19 10:02:29 +0300

    Solved task "Extract person information" from Text processing - more exercises

---
+++ 08.TextProcessing/ExtractPersonInformation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace ExtractPersonInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < count; i++)
+            {
+                string info = Console.ReadLine();
+
+                var nameStartIndex = info.IndexOf("@");
+                var nameEndIndex = info.IndexOf("|");
+                var ageStartIndex = info.IndexOf("#");
+                var ageEndIndex = info.IndexOf("*");
+
+                StringBuilder name = new StringBuilder();
+
+                for (int j = nameStartIndex + 1; j < nameEndIndex; j++)
+                    name.Append(info[j]);
+
+                StringBuilder age = new StringBuilder();
+
+                for (int k = ageStartIndex + 1; k < ageEndIndex; k++)
+                    age.Append(info[k]);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
