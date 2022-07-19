00c0b1303e2568a1a14d39fc05ee82ce7ab75e8a
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-19 12:06:16 +0300

    Solved task "Treasure hunt" from Text processing - more exercises

---
+++ 08.TextProcessing/TreasureHunt/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] keys = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string input = Console.ReadLine();
+
+            while (input!="find")
+            {
+                StringBuilder decrypted = new StringBuilder();
+                int index = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (index == keys.Length) index = 0;
+                    int key = keys[index];
+
+                    decrypted.Append((char)(input[i]-key));
+                    index++;
+                }
+
+                string decryptedText = decrypted.ToString();
+
+                Match type = Regex.Match(decryptedText, @"&(?<type>[\w]+)&");
+                Match coordinates = Regex.Match(decryptedText, @"<(?<coo>[\w]+)>");
+
+                Console.WriteLine($"Found {type.Groups["type"].Value} at {coordinates.Groups["coo"].Value}");
+
+                input = Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
