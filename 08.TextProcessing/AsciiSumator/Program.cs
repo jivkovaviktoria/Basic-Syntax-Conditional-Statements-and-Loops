84413df9fe121d658f03fbb1568218ef6d0c2e85
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-07-19 10:38:41 +0300

    Solved task "Ascii sumator" from Text processing - more exercises

---
+++ 08.TextProcessing/AsciiSumator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+
+            int start = Math.Min(char1, char2);
+            int end = Math.Max(char1, char2);
+
+            int sum = 0;
+
+            foreach (char ch in text)
+            {
+                int currentChar = (int)ch;
+                if (currentChar > start && currentChar < end) sum += currentChar;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
