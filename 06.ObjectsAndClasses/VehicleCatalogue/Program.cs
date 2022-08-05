06d275fa01782bbf119d5b47497d4712062d594c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-08-05 11:55:59 +0300

    Solved "Vehicle catalogue" from Objects and classes - exercise

--- 06.ObjectsAndClasses/VehicleCatalogue/Program.cs
+++ 06.ObjectsAndClasses/VehicleCatalogue/Program.cs
@@ -8,9 +8,11 @@ namespace VehicleCatalogue
     {
         static void Main(string[] args)
         {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
             string input = Console.ReadLine();
-            
-            while (input!="End")
+
+            while (input != "End")
             {
                 string[] tokens = input.Split();
                 string type = tokens[0];
@@ -18,22 +20,49 @@ namespace VehicleCatalogue
                 string color = tokens[2];
                 int hp = int.Parse(tokens[3]);
 
-                if(type == "Car")
-                {
-                    Car newCar = new Car(type, model, color, hp);
-                }
+                Vehicle newCar = new Vehicle(type, model, color, hp);
+                vehicles.Add(newCar);
 
                 input = Console.ReadLine();
             }
 
+            input = Console.ReadLine();
+            while (input != "Close the Catalogue")
+            {
+                Vehicle vehicle = vehicles.Find(x => x.Model == input);
+
+                Console.WriteLine($"Type: {vehicle.Type}");
+                Console.WriteLine($"Model: {vehicle.Model}");
+                Console.WriteLine($"Color: {vehicle.Color}");
+                Console.WriteLine($"Horsepower: {vehicle.HP}");
+
+                input = Console.ReadLine();
+            }
+
+            List<int> carsHP = new List<int>();
+            List<int >trucksHP = new List<int>();
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                if (vehicle.Type == "Car") carsHP.Add(vehicle.HP);
+                
+                else if (vehicle.Type == "Truck") trucksHP.Add(vehicle.HP);
+            }
+
+            if(carsHP.Count > 0) Console.WriteLine($"Cars have average horsepower of: {carsHP.Average():f2}.");
+            else Console.WriteLine($"Cars have average horsepower of: {0:f2}.");
+            if(trucksHP.Count > 0) Console.WriteLine($"Trucks have average horsepower of: {trucksHP.Average():F2}.");
+            else Console.WriteLine($"Trucks have average horsepower of: {0:f2}.");
         }
     }
 
-    class Truck
+    class Vehicle
     {
-        public Truck(string type, string model, string color, int hp)
+        public Vehicle(string type, string model, string color, int hp)
         {
-            Type = type;
+            if (type == "car") Type = "Car";
+            else Type = "Truck";
+
             Model = model;
             Color = color;
             HP = hp;
@@ -48,34 +77,4 @@ namespace VehicleCatalogue
         public int HP { get; set; }
     }
 
-    class Car
-    {
-        public Car(string type, string model, string color, int hp)
-        {
-            Type = type;
-            Model = model;
-            Color = color;
-            HP = hp;
-
-
-        }
-
-        public string Type { get; set; }
-
-        public string Model { get; set; }
-
-        public string Color { get; set; }
-
-        public int HP { get; set; }
-    }
-
-    class Catalogue
-    {
-        public List<Truck> Trucks { get; set; }
-
-        public List<Car> Cars { get; set; }
-
-        public List<int> carsHP { get; set; }
-        public List<int> trucksHP { get; set; }
-    }
 }
